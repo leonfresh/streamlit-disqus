7d040da80c2efc10ceb5168d6fb32d344e35c9c8
leonfresh <leonfreshdesign@gmail.com>
2023-06-09 02:30:01 +1000

    x

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from pathlib import Path
 README = (Path(__file__).parent/"README.md").read_text()
 
 setuptools.setup(
-    name="streamlit-disqus",
-    version="0.1.3",
-    author="okld",
+    name="streamlit-disqus-darkmode",
+    version="0.1",
+    author="leonfresh",
     author_email="",
     description="A streamlit component to embed Disqus in your applications.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/okld/streamlit-disqus",
+    url="https://github.com/leonfresh/streamlit-disqus-darkmode",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
